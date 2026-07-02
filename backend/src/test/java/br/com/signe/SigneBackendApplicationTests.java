18975b46c3f63795fb193aa15e0a13971e46dbf2
emilybook <milyymilyy.15@gmail.com>
2026-07-02 09:47:43 -0300

    feat:setup inicial dos frameworks e estruturação do diretório.

---
+++ backend/src/test/java/br/com/signe/SigneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.signe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigneBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
