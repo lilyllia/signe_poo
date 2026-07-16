f3ac587ee38c25fb3ad6dcac46fb0d99d0adfb49
yasmimonteiro <yasmimonteirogomes@gmail.com>
2026-07-15 21:11:49 -0300

    Feat: Implementação do pacote service (Enum ProcedureCategory e suas classes Procedure, Product e InventoryManagement)

---
+++ backend/src/main/java/br/com/signe/service/ProcedureCategory.java
@@ -0,0 +1,12 @@
+package br.com.signe.service;
+
+public enum ProcedureCategory {
+    BLOWOUT,
+    HAIR_TREATMENT,
+    SCALP_THERAPY,
+    CONSULTATION,
+    CHEMICAL_SERVICES,
+    HAIRCUT,
+    HAIR_COLORING,
+    NAILS;
+}
