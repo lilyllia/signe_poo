0b8b027b55b96b7df8303265cd0c54d24694ac05
pedroSampaio12 <pedrosa0335l@gmail.com>
2026-07-24 14:41:12 -0300

    Adição do Jakarta Persistence à classe Administrator

--- backend/src/main/java/br/com/signe/employee/Administrator.java
+++ backend/src/main/java/br/com/signe/employee/Administrator.java
@@ -1,6 +1,13 @@
 package br.com.signe.employee;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.DiscriminatorValue;
+
+@Entity
+@DiscriminatorValue("ADMINISTRATOR")
 public class Administrator extends Employees {
+
+    public Administrator() {}
 
     public Administrator(String name, String cpf, String id, String email, String hasPhone, String address, double baseSalary, EmployeeStatus status) {
         super(name, cpf, id, email, hasPhone, address, baseSalary, status);
